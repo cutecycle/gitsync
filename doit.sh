96e7ea811a42b1008eecfc035fdf0104b6d45022
Nina Reynolds <nina@witsmd.com>
2019-08-22 18:59:00 -0500

    feat: mvp

--- doit.sh
+++ doit.sh
@@ -1,12 +1,9 @@
 set -x
 
-rsync --exclude=".gitsync" -artv . .gitsync 
-then
-	cd .gitsync
-	git status
-	git branch -m $(hostname)-auto
-	git add . 
-	git commit -m "Auto commit"
-	git push -f --set-upstream origin $(hostname)-auto
+rsync --exclude=".gitsync" -av . .gitsync 
+cd .gitsync
+git status
+git branch -m $HOST-auto
+git add . 
+git commit -m "Auto commit" && git push -f --set-upstream origin $HOST-auto
 
-fi
