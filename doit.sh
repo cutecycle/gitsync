0014a6c408f3493660c742bdf26036f0ad008e9d
Nina Reynolds <nina@skyprint.org>
2019-08-22 23:18:04 -0500

    -b

--- doit.sh
+++ doit.sh
@@ -2,7 +2,7 @@ set -x
 rsync --exclude=".gitsync" -a . .gitsync 
 cd .gitsync
 git status
-git branch -m $HOSTNAME-auto
+git branch -b $HOSTNAME-auto
 git add . 
 git commit -m "Auto commit" && git push -f --set-upstream origin $HOSTNAME-auto
 
